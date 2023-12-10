8dc51a232f3d54d7321e993c768792737b5aba5e
林依 <linyi@linyideiMac.local>
2023-12-10 21:11:31 +0800

    update codes

---
+++ additional_results.md
@@ -0,0 +1,45 @@
+## Response: Increase in the unemployment rate in year 20
+<figure>
+  <img src="./figs/unemployment-year-continue.jpg" alt="d">
+  <figcaption style="text-align: center;">unemployment rate (additional 5 years)</figcaption>
+</figure>
+
+## Response: eCDF of tax paid and redistribution
+<figure>
+  <img src="./figs/tax_paid-work.jpg" alt="d">
+  <figcaption style="text-align: center;">coefficients of tax paied</figcaption>
+</figure>
+<figure>
+  <img src="./figs/redistribution-work.jpg" alt="d">
+  <figcaption style="text-align: center;">coefficients of redistribution</figcaption>
+</figure>
+
+## Response: Sensitivity, scalability, and reproducibility
+<figure>
+  <img src="./figs/llama-unemployment-year.jpg" alt="d">
+  <figcaption style="text-align: center;">unemployment rate by Llama-2</figcaption>
+</figure>
+<figure>
+  <img src="./figs/llama-inflation-year.jpg" alt="d">
+  <figcaption style="text-align: center;">inflation rate by Llama-2</figcaption>
+</figure>
+<figure>
+  <img src="./figs/llama-Phillips Curve.jpg" alt="d">
+  <figcaption style="text-align: center;">Phillips Curve by Llama-2</figcaption>
+</figure>
+
+## Response: Ablation study on LLM modules & Technical definitions of agents abilities
+<figure>
+  <img src="./figs/ablation-inflation-year.jpg" alt="d">
+  <figcaption style="text-align: center;">inflation rate</figcaption>
+</figure>
+<figure>
+  <img src="./figs/ablation-unemployment-year.jpg" alt="d">
+  <figcaption style="text-align: center;">unemployment rate</figcaption>
+</figure>
+
+## Response: Correlations between agents' age and job
+<figure>
+  <img src="./figs/work_after_unemployment.jpg" alt="d">
+  <figcaption style="text-align: center;">P(Work|Uemployment) for agents with different ages</figcaption>
+</figure>
--- simulate.py
+++ simulate.py
@@ -21,129 +21,9 @@ with open('config.yaml', "r") as f:
 env_config = run_configuration.get('env')
         
 def gpt_actions(env, obs, dialog_queue, dialog4ref_queue, gpt_path, gpt_error, total_cost):
-    if not os.path.exists(gpt_path):
-        os.makedirs(gpt_path)
-    curr_rates = obs['p']['PeriodicBracketTax-curr_rates']
-    current_time = world_start_time + relativedelta(months=env.world.timestep)
-    current_time = current_time.strftime('%Y.%m')
-    for idx in range(env.num_agents):
-        this_agent = env.get_agent(str(idx))
-        skill = this_agent.state['skill']
-        wealth = this_agent.inventory['Coin']
-        consumption = this_agent.consumption['Coin']
-        interest_rate = env.world.interest_rate[-1]
-        price = env.world.price[-1]
-        tax_paid = obs['p'][f'p{idx}']['PeriodicBracketTax-tax_paid']
-        lump_sum = obs['p'][f'p{idx}']['PeriodicBracketTax-lump_sum']
-        max_l = env._components_dict['SimpleLabor'].num_labor_hours
-        name = this_agent.endogenous['name']
-        age = this_agent.endogenous['age']
-        city = this_agent.endogenous['city']
-        job = this_agent.endogenous['job']
-        offer = this_agent.endogenous['offer']
-        actions = env.dense_log['actions']
-        states = env.dense_log['states']
-        problem_prompt = f'''
-                    You're {name}, a {age}-year-old individual living in {city}. As with all Americans, a portion of your monthly income is taxed by the federal government. This taxation system is tiered, income is taxed cumulatively within defined brackets, combined with a redistributive policy: after collection, the government evenly redistributes the tax revenue back to all citizens, irrespective of their earnings.
-                    Now it's {current_time}.
-                '''
-        if job == 'Unemployment':
-            job_prompt = f'''
-                        In the previous month, you became unemployed and had no income. Now, you are invited to work as a(an) {offer} with monthly salary of ${skill*max_l:.2f}.
-                    '''
-        else:
-            if skill >= states[-1][str(idx)]['skill']:
-                job_prompt = f'''
-                            In the previous month, you worked as a(an) {job}. If you continue working this month, your expected income will be ${skill*max_l:.2f}, which is increased compared to the last month due to the inflation of labor market.
-                        '''
-            else:
-                job_prompt = f'''
-                            In the previous month, you worked as a(an) {job}. If you continue working this month, your expected income will be ${skill*max_l:.2f}, which is decreased compared to the last month due to the deflation of labor market.
-                        '''
-        if (consumption <= 0) and (len(actions) > 0) and (actions[-1].get('SimpleConsumption', 0) > 0):
-            consumption_prompt = f'''
-                        Besides, you had no consumption due to shortage of goods.
-                    '''
-        else:
-            consumption_prompt = f'''
-                        Besides, your consumption was ${consumption:.2f}.
-                    '''
-        if env._components_dict['PeriodicBracketTax'].tax_model == 'us-federal-single-filer-2018-scaled':
-            tax_prompt = f'''Your tax deduction amounted to ${tax_paid:.2f}. However, as part of the government's redistribution program, you received a credit of ${lump_sum:.2f}.
-                            In this month, the government sets the brackets: {format_numbers(brackets)} and their corresponding rates: {format_numbers(curr_rates)}. Income earned within each bracket is taxed only at that bracket's rate.'''
-        else:
-            tax_prompt = f'''Your tax deduction amounted to ${tax_paid:.2f}. However, as part of the government's redistribution program, you received a credit of ${lump_sum:.2f}.
-                            In this month, according to the optimal taxation theory, Saez Tax, the brackets are not changed: {format_numbers(brackets)} but the government has updated corresponding rates: {format_percentages(curr_rates)}. Income earned within each bracket is taxed only at that bracket's rate.'''
-        if env.world.timestep == 0:
-            price_prompt = f'''Meanwhile, in the consumption market, the average price of essential goods is now at ${price:.2f}.'''
-        else:
-            if price >= env.world.price[-2]:
-                price_prompt = f'''Meanwhile, inflation has led to a price increase in the consumption market, with the average price of essential goods now at ${price:.2f}.'''
-            else:
-                price_prompt = f'''Meanwhile, deflation has led to a price decrease in the consumption market, with the average price of essential goods now at ${price:.2f}.'''
-        job_prompt = prettify_document(job_prompt)
-        obs_prompt = f'''
-                        {problem_prompt} {job_prompt} {consumption_prompt} {tax_prompt} {price_prompt}
-                        Your current savings account balance is ${wealth:.2f}. Interest rates, as set by your bank, stand at {interest_rate*100:.2f}%. 
-                        With all these factors in play, and considering aspects like your living costs, any future aspirations, and the broader economic trends, how is your willingness to work this month? Furthermore, how would you plan your expenditures on essential goods, keeping in mind good price?
-                        Please share your decisions in a JSON format. The format should have two keys: 'work' (a value between 0 and 1 with intervals of 0.02, indicating the willingness or propensity to work) and 'consumption' (a value between 0 and 1 with intervals of 0.02, indicating the proportion of all your savings and income you intend to spend on essential goods).
-                    '''
-        obs_prompt = prettify_document(obs_prompt)
-        dialog_queue[idx].append({'role': 'user', 'content': obs_prompt})
-        dialog4ref_queue[idx].append({'role': 'user', 'content': obs_prompt})
-        
-    def action_check(actions):
-        if len(actions) != 2:
-            return False
-        else:
-            return (actions[0] >= 0) & (actions[0] <= 1) & (actions[1] >= 0) & (actions[1] <= 1)
-    if env.world.timestep%3 == 0 and env.world.timestep > 0:
-        results, cost = get_multiple_completion([list(dialogs)[:2] + list(dialog4ref)[-3:-1] + list(dialogs)[-1:] for dialogs, dialog4ref in zip(dialog_queue, dialog4ref_queue)])
-        total_cost += cost
-    else:
-        results, cost = get_multiple_completion([list(dialogs) for dialogs in dialog_queue])
-        total_cost += cost
-    actions = {}
-    for idx in range(env.num_agents):
-        content = results[idx]
-        try:
-            extracted_actions = list(eval(content).values())
-            if not action_check(extracted_actions):
-                extracted_actions = [1, 0.5]
-                gpt_error += 1
-        except:
-            extracted_actions = [1, 0.5]
-            gpt_error += 1
-        extracted_actions[0] = int(np.random.uniform() <= extracted_actions[0])
-        extracted_actions[1] /= 0.02
-        actions[str(idx)] = extracted_actions
-        dialog_queue[idx].append({'role': 'assistant', 'content': f'{content}'})
-        dialog4ref_queue[idx].append({'role': 'assistant', 'content': f'{content}'})
-    actions['p'] = [0]
-    for idx, agent_dialog in enumerate(dialog_queue):
-        with open(f'''{gpt_path}/{env.get_agent(str(idx)).endogenous['name']}''', 'a') as f:
-            for dialog in list(agent_dialog)[-2:]:
-                f.write(f'''>>>>>>>>>{dialog['role']}: {dialog['content']}\n''')
-        
-    if (env.world.timestep+1)%3 == 0:
-        reflection_prompt = '''Given the previous quarter's economic environment, reflect on the labor, consumption, and financial markets, as well as their dynamics. What conclusions have you drawn?
-        Your answer must be less than 200 words!'''
-        reflection_prompt = prettify_document(reflection_prompt)
-        for idx in range(env.num_agents):
-            # dialog_queue[idx].append({'role': 'user', 'content': reflection_prompt})
-            dialog4ref_queue[idx].append({'role': 'user', 'content': reflection_prompt})
-        results, cost = get_multiple_completion([list(dialogs) for dialogs in dialog4ref_queue], temperature=0, max_tokens=200)
-        total_cost += cost
-        for idx in range(env.num_agents):
-            content = results[idx]
-            # dialog_queue[idx].append({'role': 'assistant', 'content': content})
-            dialog4ref_queue[idx].append({'role': 'assistant', 'content': content})
-        
-        for idx, agent_dialog in enumerate(dialog4ref_queue):
-             with open(f'''{gpt_path}/{env.get_agent(str(idx)).endogenous['name']}''', 'a') as f:
-                for dialog in list(agent_dialog)[-2:]:
-                    f.write(f'''>>>>>>>>>{dialog['role']}: {dialog['content']}\n''')
-    return actions, gpt_error, total_cost
+    # To update after acceptance
+    pass
+    # return actions, gpt_error, total_cost
 
 def complex_actions(env, obs, beta=0.1, gamma=0.1, h=1):
 
@@ -188,7 +68,7 @@ def complex_actions(env, obs, beta=0.1, gamma=0.1, h=1):
     return actions
     
 
-def main(policy_model='gpt', num_agents=100, episode_length=240, dialog_len=3, beta=0.1, gamma=0.1, h=1, max_price_inflation=0.1, max_wage_inflation=0.05):
+def main(policy_model='complex', num_agents=100, episode_length=240, dialog_len=3, beta=0.1, gamma=0.1, h=1, max_price_inflation=0.1, max_wage_inflation=0.05):
     env_config['n_agents'] = num_agents
     env_config['episode_length'] = episode_length
     if policy_model == 'gpt':
